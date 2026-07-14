eb0dc9f8a09fd9e78776e8d55d38645b26a92af6
Raihansome12 <raihanahmad3012@gmail.com>
2026-07-15 04:08:19 +0700

    creating user admin dataseeder

---
+++ database/seeders/UserDataSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserDataSeeder extends Seeder
+{
+    public function run(): void
+    {
+        \App\Models\User::updateOrCreate(
+            ['email' => 'stageof.nabire@bmkg.go.id'],
+            [
+                'name' => 'Admin',
+                'password' => json_encode('Geofnabire25!'),
+                'is_admin' => 1,
+            ]
+        );
+    }
+}
